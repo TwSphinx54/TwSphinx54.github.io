8f837d6e022782eafb4c3467167f9ef1936bb651
Xu Pan <panxurs@whu.edu.cn>
2026-08-07 01:32:30 +0800

    Add creative gallery and photography

--- src/site.config.ts
+++ src/site.config.ts
@@ -22,6 +22,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		title: "Publications",
 	},
 	{
+		path: "/creative/",
+		title: "Creative",
+	},
+	{
 		path: "/contact/",
 		title: "Contact",
 	},
